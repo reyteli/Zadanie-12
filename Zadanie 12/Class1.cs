b059785f3645dd0c2a1b1c731827b7834cda1ba7
Никита Олегович Романенко <nikcom526@mail.ru>
2024-01-13 19:50:15 +0300

    12

---
+++ Zadanie 12/Class1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadanie_12
+{
+    static class Cycle
+    {
+        static public double GetSquare(double r)
+        {
+            return r * r * Math.PI;
+        }
+
+
+        static public double GetDlina(double r)
+        {
+            return 2 * (r * Math.PI);
+        }
+        static public double GetProverka(double r,double x, double y)
+        {
+            return Math.Sqrt(Math.Pow(0 - x, 2) + Math.Pow(0 - y, 2));
+
+        }
+    }
+}
+    
+
+
+
+
+
+
---
+++ Zadanie 12/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+
+namespace Zadanie_12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("введите r");
+            double r = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("введите x");
+            double x = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("введите y");
+            double y = Convert.ToDouble(Console.ReadLine());
+            double square = Cycle.GetSquare(r);
+            double dlina = Cycle.GetDlina(r);
+            double proverka = Cycle.GetProverka(r,x,y);
+            if (proverka <= r)
+                Console.WriteLine("Точка М лежит в круге.");
+            else
+                Console.WriteLine("Точка М лежит вне круга.");
+            Console.WriteLine($"{square} \n{dlina}");
+            Console.ReadKey();
+        }
+    }
+}
